7e772687768dad674888a2e8e5aecad57063e487
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-05-18 00:49:24 -0600

    more dom

--- src/steamer/dom/Dom.hx
+++ src/steamer/dom/Dom.hx
@@ -2,10 +2,11 @@ package steamer.dom;
 
 import js.html.Element;
 import js.html.Event;
+import steamer.Consumer;
 import steamer.Producer;
 
 class Dom {
-	public static function emit(el : Element, name : String) : CancellableProducer<Event> {
+	public static function produceEvent(el : Element, name : String) : CancellableProducer<Event> {
 		var cancel = null;
 		return new CancellableProducer(
 			function(forward) {
@@ -22,5 +23,36 @@ class Dom {
 				cancel();
 			}
 		);
+	}
+
+	public static function consumeText(el : Element) : Consumer<String>
+		return createConsumer(function(v) el.innerText = v);
+
+	public static function consumeHtml(el : Element) : Consumer<String>
+		return createConsumer(function(v) el.innerHTML = v);
+
+	public static function consumeAttribute<T>(el : Element, attr : String) : Consumer<String>
+		return createConsumer(function(v) el.setAttribute(attr, v));
+
+	public static function consumeToggleClass<T>(el : Element, name : String) : Consumer<Bool>
+		return createConsumer(function(v) {
+			if(v)
+				el.classList.add(name);
+			else
+				el.classList.remove(name);
+		});
+
+	static function createConsumer<T>(f : T -> Void) {
+		return {
+			onPulse: function(pulse : Pulse<T>) {
+				switch(pulse) {
+					case Emit(v):
+						f(v);
+					case End:
+					case Fail(error):
+						throw error;
+				}
+			}
+		}
 	}
 }
