001e716decfc6785c289fe3a313e90797f6e532e
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-06-20 23:26:44 +0200

    improved error

--- src/thx/Error.hx
+++ src/thx/Error.hx
@@ -1,7 +1,24 @@
 package thx;
 
-@:native("Error")
-extern class Error {
-	public function new(message : String) : Void;
-	public var message : String;
+import haxe.PosInfos;
+import haxe.CallStack;
+
+class Error {
+	public var message(default, null) : String;
+	public var stack(default, null) : Array<StackItem>;
+	public var pos(default, null) : PosInfos;
+	public function new(message : String, ?stack : Array<StackItem>, ?pos : PosInfos) {
+		this.message = message;
+		if(null == stack) {
+			stack = CallStack.exceptionStack();
+			if(stack.length == 0)
+				stack = CallStack.callStack();
+		}
+		this.stack = stack;
+		this.pos = pos;
+	}
+
+	public function toString() {
+		return message + "from: " + pos.className + "." + pos.methodName + "() at " + pos.lineNumber + "\n\n" + CallStack.toString(stack);
+	}
 }
